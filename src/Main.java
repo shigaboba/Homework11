b4cd435ccbac8663dd01adc9f5c7bbc32e96d9c2
shigaboba <shigapovalrm@gmail.com>
2023-01-31 00:27:39 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import java.time.LocalDate;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+    public static String isYearLeap (int year) {
+        String result = " ";
+        if ( year%4 == 0 && year%100 != 0) {
+            result = " год — високосный год";
+        }
+        else if ( year%400 == 0 ) {
+            result = " год — високосный год";
+        }
+        else {
+            result = " год — невисокосный год";
+        }
+        return result;
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int year = 1995;
+        System.out.println(year +  isYearLeap(year));
+    }
+
+    public static String chooseVersion (int clientOS, int clientDeviceYear) {
+        String result;
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0) {
+            result = "Установите версию приложения для iOS по ссылке";
+            if (clientDeviceYear < currentYear) {
+                result = "Установите облегченную версию приложения для iOS по ссылке";
+            }
+        } else {
+            result = "Установите версию приложения для Android по ссылке";
+            if (clientDeviceYear < currentYear) {
+                result = "Установите облегченную версию приложения для Android по ссылке";
+            }
+        }
+        return result;
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int clientOS = 0;
+        int clientDeviceYear = 2014;
+        System.out.println(chooseVersion(clientOS, clientDeviceYear));
+    }
+
+    public static int countDays (int deliveryDistance) {
+        int daysOfDelivery = 0;
+        if ( deliveryDistance < 20 ) {
+            daysOfDelivery = 1;
+        }
+        if ( deliveryDistance >= 20 && deliveryDistance < 60 ) {
+            daysOfDelivery = 2;
+        }
+        if ( deliveryDistance >= 60 && deliveryDistance < 100 ) {
+            daysOfDelivery = 3;
+        }
+        return daysOfDelivery;
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+        int deliveryDistance = 95;
+        if ( deliveryDistance >= 100) {
+            System.out.println("Доставки нет");
+        }
+        else {
+            System.out.println("Потребуется дней: " + countDays(deliveryDistance));
+        }
+    }
+
 }
